8fc50d243a6214a9b1214f4e0060582ac00a99d8
Kunal-KD <93980019+Kunal-KD@users.noreply.github.com>
2021-11-09 15:46:31 +0530

    Add files via upload

---
+++ conditionalLab7.py
@@ -0,0 +1,6 @@
+"""
+Your module description
+"""
+kunal = [1,2,3,4]
+for a in kunal:
+    print("naruto")
---
+++ mycode1.py
@@ -0,0 +1,8 @@
+"""
+Your module description
+"""
+name = "Kunal"
+age = 21
+salary = 25000
+print ("My name is " + name + ".", "\nMy age is " + str(age) + ".", "\nMy monthly salary is " + str(salary)+".")
+print ("My name is " + name+".", "My age is " + str(age)+".", "My monthly salary is " + str(salary)+".")
